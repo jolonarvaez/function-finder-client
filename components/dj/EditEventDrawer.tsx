c7cb73cfbe93b062961f87003739bf6302077901
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2026-04-06 20:37:37 +0800

    update edit event drawer

--- components/dj/EditEventDrawer.tsx
+++ components/dj/EditEventDrawer.tsx
@@ -180,7 +180,7 @@ export function EditEventDrawer({ event, onSave, onClose }: EditEventDrawerProps
               </div>
 
               {/* Start / End time */}
-              <div className="grid grid-cols-2 gap-3">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
                 <div className="space-y-1.5">
                   <FieldLabel>Start</FieldLabel>
                   <Input
